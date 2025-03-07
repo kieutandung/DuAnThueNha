6a8b21e58207d88175226af581f7c2109bfa718e
Trangne2901 <tranthuyentrang29012005@gmail.com>
2025-03-07 10:13:51 +0700

    a

--- IdeaProjects/DuAnThueNha/src/main/java/com/example/duanthuenha/ConnectDB/ConnectDB.java
+++ IdeaProjects/DuAnThueNha/src/main/java/com/example/duanthuenha/ConnectDB/ConnectDB.java
@@ -11,7 +11,7 @@ public class ConnectDB {
 //    private String password = System.getenv("jdbcPasswordDB");
 
     private String username = "root";
-    private String password = "13122005";
+    private String password = "1234";
     public Connection getConnection() {
         Connection connection = null;
         try {
