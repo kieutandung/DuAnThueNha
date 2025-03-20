1a578cc7368c2b42dd4a5d3bf75df13ad0e4f666
kieutandung <tandungkieu1312@gmail.com>
2025-03-20 10:05:30 +0700

    fix

--- IdeaProjects/DuAnThueNha/src/main/java/com/example/duanthuenha/Controller/Admin/AdminServlet.java
+++ IdeaProjects/DuAnThueNha/src/main/java/com/example/duanthuenha/Controller/Admin/AdminServlet.java
@@ -41,8 +41,8 @@ public class AdminServlet extends HttpServlet {
                 case "editUser":
                     handleEditUserView(req, resp);
                     break;
-                case "revenueChart":
-                    revenueChart(req, resp);
+                case "accountManagement":
+                    listAccountView(req, resp);
                     break;
                 case "reportView":
                     listReportView(req, resp);
@@ -51,7 +51,7 @@ public class AdminServlet extends HttpServlet {
                     listReportViewPending(req, resp);
                     break;
                 default:
-                    listAccountView(req, resp);
+                    revenueChart(req, resp);
                     break;
             }
         } catch (ServletException e) {
@@ -178,8 +178,11 @@ public class AdminServlet extends HttpServlet {
                 case "sendFeedback":
                     sendFeedback(req, resp);
                     break;
-                default:
+                case "accountManagement":
                     listAccountView(req, resp);
+                    break;
+                default:
+                    revenueChart(req, resp);
                     break;
             }
         } catch (ServletException e) {
